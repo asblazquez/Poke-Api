0c17d440148286b8d5fd58596ae829308b075b85
Andres Sanchez <asanchezb23@gmail.com>
2022-08-01 14:40:11 +0200

    NPM Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div>
-      Ricardo huele mal pero mal de mal y gallo es gay
+      <button className='btn btn-primary'> Prueba </button>
     </div>
   );
 }
